b7db8d38931a2c01fa6557af7e8b5709c55a60b7
Glen Worsley <glen.worsley@gmail.com>
2020-04-21 17:05:41 +1000

    setting up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-southeast-2",
+    BUCKET: "notes-app-uploads-glenworsley-232322",
+  },
+  apiGateway: {
+    REGION: "ap-southeast-2",
+    URL: "https://j34hb3sxfb.execute-api.ap-southeast-2.amazonaws.com/dev",
+  },
+  cognito: {
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_Ndqvoinbj",
+    APP_CLIENT_ID: "17rc7557omdror5j4ma48ijk2c",
+    IDENTITY_POOL_ID: "ap-southeast-2:123f8f25-1268-4daa-95ea-b6027fea50af",
+  },
+};
